aecf56f2d4e8c7d2ec97605ac7737f4a7875f966
James Viray <jamesviray6969@gmail.com>
2026-04-10 21:50:16 +0800

    new repository in navigation

--- app/src/main/java/com/example/iskolarphh/MainActivity.java
+++ app/src/main/java/com/example/iskolarphh/MainActivity.java
@@ -1,23 +1,57 @@
 package com.example.iskolarphh;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.iskolarphh.repository.CatalogFragment;
+import com.example.iskolarphh.repository.DashboardFragment;
+import com.example.iskolarphh.repository.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            int id = item.getItemId(); // ✅ added
+
+            if (id == R.id.nav_home) {
+                selectedFragment = new DashboardFragment();
+
+            } else if (id == R.id.nav_search) {
+                selectedFragment = new CatalogFragment();
+
+            } else if (id == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+
+            return true;
+        }
+    };
 }
